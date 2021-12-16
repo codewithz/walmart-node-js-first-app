fe10f1de6afde8b766c9fe331e524a393e7fbcf5
Zartab Nakhwa <zartab@codewithz.com>
2021-12-16 09:36:06 +0530

    Reading file in async way

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 
 console.log('Start');
 
-const data = fs.readFileSync('./walmart.txt');// block here until file read is complete
-console.log(data);
+// const data = fs.readFileSync('./walmart.txt');// block here until file read is complete
+// console.log(data);
+
+fs.readFile('./walmart.txt', { encoding: 'utf-8' }, (err, data) => {
+    if (err) console.log(err);
+    console.log(data);
+})
 
 console.log('End');
