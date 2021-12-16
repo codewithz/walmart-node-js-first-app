6ce40fb45dcd4a39ac4beeff8c9f827560d4c868
Zartab Nakhwa <zartab@codewithz.com>
2021-12-16 12:31:16 +0530

    Replaced Callbacks with Promises

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
 console.log('Before');
 //Asynchronous way
-getCategory(100, getTopTwoProductsNamed);
-
-
-
-function getTopTwoProductsNamed(category) {
-
+getCategory(100, function (category) {
     console.log('Category', category);
     //Getting the top two products
-    getTopTwoProducts(category.name, getPriceForTopProductNamed);
-
-}
-
-function getPriceForTopProductNamed(topTwoProducts) {
-    console.log(topTwoProducts);
-    // Get the price for first products
-    getPriceForTopProduct(topTwoProducts[0], displayPriceOfTopProduct);
-}
-
-function displayPriceOfTopProduct(price) {
-    console.log("Price of the product is:" + price);
-}
-
+    getTopTwoProducts(category.name, function (topTwoProducts) {
+        console.log(topTwoProducts);
+        // Get the price for first products
+        getPriceForTopProduct(topTwoProducts[0], function (price) {
+            console.log("Price of the product is:" + price);
+        });
+    });
+});
 
 
 console.log('After');
 
 
-function getCategory(id, callback) {
+function getCategory(id) {
+    return new Promise((resolve, reject) => {
 
-    setTimeout(() => {
-        console.log('Reading a category from database...');
-        callback({ categoryId: id, name: 'Electronics' });
-    }, 2000);
+        setTimeout(() => {
+            console.log('Reading a category from database...');
+            resolve({ categoryId: id, name: 'Electronics' });
+        }, 2000);
+
+    });
 
 }
 
-function getTopTwoProducts(category, callback) {
-    setTimeout(() => {
-        console.log('Calling the Walmart API for getting top two products in category [' + category + ']');
-        topTwoProducts = ['Apple IPhone', 'Samsung Galaxy A72'];
-        callback(topTwoProducts);
-    }, 2000)
+function getTopTwoProducts(category) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('Calling the Walmart API for getting top two products in category [' + category + ']');
+            topTwoProducts = ['Apple IPhone', 'Samsung Galaxy A72'];
+            resolve(topTwoProducts);
+        }, 2000);
+    })
 }
 
-function getPriceForTopProduct(product, callback) {
-    setTimeout(() => {
-        console.log('Fetching the price for ', product);
-        callback(5000);
-    }, 3000)
+function getPriceForTopProduct(product) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('Fetching the price for ', product);
+            resolve(5000);
+        }, 3000)
+    })
 }
