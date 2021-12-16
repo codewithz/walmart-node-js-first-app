6bd8b5da1981a196f6828234e5f225c45e1782a2
Zartab Nakhwa <zartab@codewithz.com>
2021-12-16 10:36:25 +0530

    Using Event Arguments

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const emitter = new EventEmitter();
 
 //Register a listener
 
-emitter.addListener('messageLogged', function () {
-    console.log('Listener is called')
+emitter.addListener('messageLogged', function (arg) {
+    console.log('Listener is called', arg);
 });
 
 //Raise an event 
-emitter.emit('messageLogged');    // Making a noise, produce -- signalling something has happened
+emitter.emit('messageLogged', { id: 1, url: 'https://wwww.walmart.com' });    // Making a noise, produce -- signalling something has happened
